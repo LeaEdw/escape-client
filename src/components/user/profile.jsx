a8c0147f5876772983362716be9c14c4f5334bdb
Lea Edwards <leaxedw@gmail.com>
2026-07-06 17:38:49 -0500

    added placeholders for profile page

--- src/components/user/profile.jsx
+++ src/components/user/profile.jsx
@@ -1,20 +1,24 @@
-export const UserProfile = () => {
+import "./profile.css";
+
+export const UserProfileComponent = () => {
   return (
-    <>
+    <div className="profile-container">
       <div className="top-container">
         <div className="game-badges"></div>
         <div className="user-info">
-            <div>Profile Image</div>
-            <div>Username</div>
-            <div>Edit Profile</div>
+          <div className="profile-img">Profile Image</div>
+          <div className="button-container">
+            <button className="user-button">Username</button>
+            <button className="user-button">Edit Profile</button>
+          </div>
         </div>
       </div>
       <div className="bottom-container">
-        <div className="about-me"></div>
-        <div className="favorite-game"></div>
-        <div className="best-time"></div>
-        <div className="wish-item"></div>
+        <div className="about-me">About Me</div>
+        <div className="favorite-game">Favorite Game</div>
+        <div className="best-time">Best Time</div>
+        <div className="wish-item">Wants to play next: ...</div>
       </div>
-    </>
+    </div>
   );
 };
--- src/views/applicationViews.jsx
+++ src/views/applicationViews.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/user/home";
 import { NewGameForm } from "../pages/admin/newGame"
 import { EditGameFrom } from "../pages/admin/editGame";
-import { UserProfile } from "../pages/user/userProfile";
 import { LeaderBoard } from "../pages/user/leaderboard";
+import { UserProfile } from "../pages/user/userProfile"
 
 
 export const ApplicationViews = () => {
